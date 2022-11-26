6762f61c0443d16c7a7d627babf1143937390757
AdnanOlarmmi <olarmmi@gmail.com>
2022-11-26 11:39:00 +0100

    Preview

---
+++ build/precache-manifest.b80647dcb11f440ee0b48eb177a8eddc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2294d5b12cfa5655515a",
+    "url": "/climate-app/static/css/main.9df9941f.chunk.css"
+  },
+  {
+    "revision": "2294d5b12cfa5655515a",
+    "url": "/climate-app/static/js/main.fd806288.chunk.js"
+  },
+  {
+    "revision": "91c249440c61dcae2d3e",
+    "url": "/climate-app/static/js/runtime~main.d23ed79c.js"
+  },
+  {
+    "revision": "d41f4e7fb8a54e265367",
+    "url": "/climate-app/static/js/2.11c98c66.chunk.js"
+  },
+  {
+    "revision": "ac7b2b20a6826c9f487325e4107a3cc1",
+    "url": "/climate-app/index.html"
+  }
+];
